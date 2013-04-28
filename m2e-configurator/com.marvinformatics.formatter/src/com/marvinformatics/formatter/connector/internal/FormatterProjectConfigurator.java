65a9b89d2d1fc16837da6df403f653c82144b5c7
Marvin Froeder <marvin@contaazul.com>
2013-04-28 11:06:57 -0300

    Now using osgi preferences service

--- m2e-configurator/com.marvinformatics.formatter/src/com/marvinformatics/formatter/connector/internal/FormatterProjectConfigurator.java
+++ m2e-configurator/com.marvinformatics.formatter/src/com/marvinformatics/formatter/connector/internal/FormatterProjectConfigurator.java
@@ -1,19 +1,23 @@
 package com.marvinformatics.formatter.connector.internal;
 
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.maven.plugin.MojoExecution;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.m2e.core.lifecyclemapping.model.IPluginExecutionMetadata;
 import org.eclipse.m2e.core.project.IMavenProjectFacade;
@@ -21,8 +25,31 @@ import org.eclipse.m2e.core.project.MavenProjectChangedEvent;
 import org.eclipse.m2e.core.project.configurator.AbstractBuildParticipant;
 import org.eclipse.m2e.core.project.configurator.AbstractProjectConfigurator;
 import org.eclipse.m2e.core.project.configurator.ProjectConfigurationRequest;
+import org.osgi.service.prefs.Preferences;
+
+import com.marvinformatics.formatter.connector.FormatterCore;
 
 public class FormatterProjectConfigurator extends AbstractProjectConfigurator {
+
+	public enum Formatter {
+		JAVA("configFile", "src/config/eclipse/formatter/java.xml");
+
+		private final String configuratioName;
+		private final String defaultPath;
+
+		private Formatter(String configuratioName, String defaultPath) {
+			this.configuratioName = configuratioName;
+			this.defaultPath = defaultPath;
+		}
+
+		public String getConfigurationName() {
+			return this.configuratioName;
+		}
+
+		public String getDefaultPath() {
+			return this.defaultPath;
+		}
+	}
 
 	@Override
 	public AbstractBuildParticipant getBuildParticipant(
@@ -37,53 +64,109 @@ public class FormatterProjectConfigurator extends AbstractProjectConfigurator {
 			IProgressMonitor monitor) throws CoreException {
 		IProject eclipseProject = request.getProject();
 
+		printSettings();
 		if (eclipseProject.hasNature(JavaCore.NATURE_ID)) {
-			IJavaProject javaProject = JavaCore.create(eclipseProject);
+			Xpp3Dom[] settings = parseConfigurationFile(request, monitor);
 
-			IMavenProjectFacade mavenProject = request.getMavenProjectFacade();
-			List<MojoExecution> executions = mavenProject.getMojoExecutions(
-					"com.marvinformatics.formatter", "formatter-maven-plugin",
-					monitor, "validate");
-
-			MojoExecution execution = executions.get(0);
-			Xpp3Dom cfg = execution.getConfiguration();
-			String javaConfigFile = cfg.getChild("configFile").getValue();
-			if (javaConfigFile == null
-					|| "${configfile}".equals(javaConfigFile))
-				javaConfigFile = "src/config/eclipse/formatter/java.xml";
-
-			IFile cfgFile = eclipseProject.getFile(javaConfigFile);
-
-			if (!cfgFile.exists())
-				return;
-
-			InputStream content = cfgFile.getContents();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(
-					content));
-			String line = null;
-			Map<String, String> opts = javaProject.getOptions(false);
-			try {
-				while ((line = reader.readLine()) != null) {
-					// <setting
-					// id="org.eclipse.jdt.core.formatter.comment.insert_new_line_before_root_tags"
-					// value="insert" />
-					if (!line.contains("<setting "))
-						continue;
-
-					int first = line.indexOf("id=\"") + 4;
-					String id = line.substring(first, line.indexOf('"', first));
-					first = line.indexOf("value=\"") + 7;
-					String value = line.substring(first,
-							line.indexOf('"', first));
-					opts.put(id, value);
-				}
-			} catch (IOException e) {
+			for (Xpp3Dom setting : settings) {
+				Platform.getPreferencesService()
+						.getRootNode()
+						.node("project")
+						.node(eclipseProject.getName())
+						.node(JavaCore.PLUGIN_ID)
+						.put(setting.getAttribute("id"),
+								setting.getAttribute("value"));
 			}
 
-			javaProject.setOptions(opts);
+			Platform.getPreferencesService().getRootNode().node("project")
+					.node(eclipseProject.getName()).node("org.eclipse.jdt.ui")
+					.put("cleanup.format_source_code", "true");
 		}
+
 		// jsdtConfigFile = cfg.getChild("configJsFile").getValue();
 		// src/config/eclipse/formatter/javascript.xml
+	}
+
+	private Xpp3Dom[] parseConfigurationFile(
+			ProjectConfigurationRequest request, IProgressMonitor monitor)
+			throws CoreException {
+		InputStream content = readConfigFile(Formatter.JAVA, request, monitor);
+		Xpp3Dom dom;
+		try {
+			dom = Xpp3DomBuilder.build(content, "UTF-8");
+		} catch (XmlPullParserException e) {
+			throw new CoreException(new Status(Status.ERROR,
+					FormatterCore.PLUGIN_ID, "Invalid configuration XML", e));
+		} catch (IOException e) {
+			throw new CoreException(new Status(Status.ERROR,
+					FormatterCore.PLUGIN_ID,
+					"Unable to read configuration XML", e));
+		}
+		Xpp3Dom[] settings = dom.getChild("profile").getChildren("setting");
+		return settings;
+	}
+
+	private InputStream readConfigFile(Formatter formatter,
+			ProjectConfigurationRequest request, IProgressMonitor monitor)
+			throws CoreException {
+		IMavenProjectFacade mavenProject = request.getMavenProjectFacade();
+		List<MojoExecution> executions = mavenProject.getMojoExecutions(
+				"com.marvinformatics.formatter", "formatter-maven-plugin",
+				monitor, "validate");
+
+		MojoExecution execution = executions.get(0);
+		Xpp3Dom cfg = execution.getConfiguration();
+		String javaConfigFile = cfg.getChild(formatter.getConfigurationName())
+				.getValue();
+		if (javaConfigFile == null
+				|| javaConfigFile.equalsIgnoreCase("${"
+						+ formatter.getConfigurationName() + "}"))
+			javaConfigFile = formatter.getDefaultPath();
+
+		IFile cfgFile = request.getProject().getFile(javaConfigFile);
+
+		if (!cfgFile.exists())
+			throw new CoreException(new Status(IStatus.CANCEL,
+					FormatterCore.PLUGIN_ID, "Configuration file not found!"));
+
+		InputStream content = cfgFile.getContents();
+		return content;
+	}
+
+	private void printSettings() {
+		StringBuilder sb = new StringBuilder();
+		Preferences prefs = Platform.getPreferencesService().getRootNode();
+		try {
+			eval(prefs, "\t", sb);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			File f = new File("tree.txt");
+			FileWriter fw = new FileWriter(f);
+			fw.write(sb.toString().toCharArray());
+			fw.close();
+			f.getAbsolutePath();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+	private void eval(Preferences prefs, String spacer, StringBuilder sb)
+			throws Exception {
+		String[] children = prefs.childrenNames();
+		for (String child : children) {
+			sb.append(spacer).append(child).append("\n");
+			eval(prefs.node(child), spacer + "\t", sb);
+		}
+		String[] keys = prefs.keys();
+		for (String key : keys) {
+			sb.append(spacer).append(" * ").append(key).append(": ")
+					.append(prefs.get(key, null)).append("\n");
+		}
 	}
 
 	private Map<? extends String, ? extends String> readFromCfg() {
