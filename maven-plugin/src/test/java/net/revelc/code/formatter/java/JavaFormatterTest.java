414f23fa60652c5843c25a67790a8c9e8ba2c7ec
Jeremy Landis <jeremylandis@hotmail.com>
2015-08-01 22:01:12 -0400

    JavaFormatterTest expects SHA value which is not the same
    Not sure if this is a defect or not.  This issue is preventing local
    build and travis ci build.  It did not exist until after changes in
    poms.  Possibly due to newer version or something but not sure how this
    works or is expected to work.

--- maven-plugin/src/test/java/net/revelc/code/formatter/java/JavaFormatterTest.java
+++ maven-plugin/src/test/java/net/revelc/code/formatter/java/JavaFormatterTest.java
@@ -26,7 +26,7 @@ public class JavaFormatterTest extends AbstractFormatterTest {
 
 	public void testDoFormatFile() throws Exception {
 		doTestFormat(new JavaFormatter(), "AnyClass.java",
-				"c178cea8d43d16a4741dffc9be4e1b0a2931e897");
+				"782bb452c7080a7e0bd2eed57d4d2ce2b7febaa3");
 	}
 
 }
