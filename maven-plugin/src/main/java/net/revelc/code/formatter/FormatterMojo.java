3941293dfbc1c0c597c3564fc367a9ce6be5ccf0
Marvin Froeder <velo.br@gmail.com>
2015-08-08 10:42:47 +1200

    Restored the hability to configure sourceDirectories

--- maven-plugin/src/main/java/net/revelc/code/formatter/FormatterMojo.java
+++ maven-plugin/src/main/java/net/revelc/code/formatter/FormatterMojo.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *	  http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -94,13 +94,13 @@ public class FormatterMojo extends AbstractMojo implements ConfigurationSource {
 	/**
 	 * Project's source directory as specified in the POM.
 	 */
-	@Parameter(defaultValue = "${project.build.sourceDirectory}", readonly = true, required = true)
+	@Parameter(defaultValue = "${project.build.sourceDirectory}", property = "sourceDirectory", required = true)
 	private File sourceDirectory;
 
 	/**
 	 * Project's test source directory as specified in the POM.
 	 */
-	@Parameter(defaultValue = "${project.build.testSourceDirectory}", readonly = true, required = true)
+	@Parameter(defaultValue = "${project.build.testSourceDirectory}", property = "testSourceDirectory", required = true)
 	private File testSourceDirectory;
 
 	/**
