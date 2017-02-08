bb63acf3e699c8d028f7a42e8b8298c6a6966ac1
Marvin Froeder <marvin.froeder@vizexplorer.com>
2017-02-08 18:31:14 +1300

    Fix headers

--- maven-plugin-its/src/test/java/com/marvinformatics/formatter/FormatterTest.java
+++ maven-plugin-its/src/test/java/com/marvinformatics/formatter/FormatterTest.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2010 Marvin Herman Froeder (marvin@marvinformatics.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.marvinformatics.formatter;
 
 import java.io.File;
@@ -14,37 +29,28 @@ import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
 
 @RunWith(MavenJUnitTestRunner.class)
 @MavenVersions({ "3.0.5", "3.2.5", "3.3.9" })
-public class FormatterTest
-{
+public class FormatterTest {
 
-  @Rule
-  public final TestResources resources = new TestResources();
+	@Rule
+	public final TestResources resources = new TestResources();
 
-  public final MavenRuntime maven;
+	public final MavenRuntime maven;
 
-  public FormatterTest(MavenRuntimeBuilder mavenBuilder) throws Exception
-  {
-    this.maven = mavenBuilder.withCliOptions("-B").build();
-  }
+	public FormatterTest(MavenRuntimeBuilder mavenBuilder) throws Exception {
+		this.maven = mavenBuilder.withCliOptions("-B").build();
+	}
 
-  @Test
-  public void simpleFormat() throws Exception
-  {
-    File basedir = resources.getBasedir("format");
-    maven.forProject(basedir)
-        .withCliOption("-X")
-        .execute("formatter:format")
-        .assertErrorFreeLog()
-        .assertLogText("Successfully formatted: 1 file(s)");
-  }
+	@Test
+	public void simpleFormat() throws Exception {
+		File basedir = resources.getBasedir("format");
+		maven.forProject(basedir).withCliOption("-X").execute("formatter:format").assertErrorFreeLog()
+				.assertLogText("Successfully formatted: 1 file(s)");
+	}
 
-  @Test
-  public void validate() throws Exception
-  {
-    File basedir = resources.getBasedir("format");
-    maven.forProject(basedir)
-        .withCliOption("-X")
-        .execute("formatter:validate")
-        .assertLogText("Format doesn't match!");
-  }
+	@Test
+	public void validate() throws Exception {
+		File basedir = resources.getBasedir("format");
+		maven.forProject(basedir).withCliOption("-X").execute("formatter:validate")
+				.assertLogText("Format doesn't match!");
+	}
 }
