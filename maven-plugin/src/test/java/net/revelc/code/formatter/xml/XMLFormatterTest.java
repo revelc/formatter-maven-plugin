6c2a8b2b65d114f685adf17dd33adb9342cccae9
Christopher Tubbs <ctubbsii@apache.org>
2017-10-26 23:22:50 -0400

    Fix #244 Correct/Improve license documentation
    * Document Apache License where appropriate instead of incorrect EPL 1.0
    * Fix file comments in java files (use single star comment blocks when
    comments are not javadocs)
    * Simplify license headers by consolidating copyright notices into the
    NOTICE file (recommended for Apache-licensed code), which makes it
    easier on those who redistribute to comply with Apache license
    requirements; also easier for project maintainers to update copyright
    dates
    * Update hashes for tests, when where license header changes affected
    the formatting

--- maven-plugin/src/test/java/net/revelc/code/formatter/xml/XMLFormatterTest.java
+++ maven-plugin/src/test/java/net/revelc/code/formatter/xml/XMLFormatterTest.java
@@ -1,7 +1,4 @@
-/**
- * Copyright 2010-2017. All work is copyrighted to their respective
- * author(s), unless otherwise stated.
- *
+/*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -35,7 +32,7 @@ public class XMLFormatterTest extends AbstractFormatterTest {
         // FIXME Handle linux vs windows since this formatter does not accept line endings
         if (SystemUtil.LINE_SEPARATOR.equals("\n")) {
             doTestFormat(new XMLFormatter(), "someFile.xml",
-                    "5b37e98476e050998ecad303cc4a3feaca45eb6966e3a7248964df2e670403939b153b45292074e926c1c22c8264df204f0c0011d6c31102652b732186868563");
+                    "ecf687f06e4ada957478267eaf9b3f90461ad2520af37e304400c75e48b3b4daa3e0be60145b76061c496a19df1ce1aa064abc91224d79a725e5cefd12367401");
         } else {
             doTestFormat(new XMLFormatter(), "someFile.xml",
                     "98f896736377248255739514b27e5cad99df44e5daa37664dc8eeb79cdeb3ec113f390247c5573e0713258e8a5da69f8e4078cf2535235e437db451803c2971c");
