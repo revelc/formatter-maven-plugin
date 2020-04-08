ccb991e9f49b44ff10d020e86b48683d5262915e
tpontz <tobias.pontz@googlemail.com>
2020-04-08 22:16:25 +0200

    make 'excludes' a property (#332)

--- src/main/java/net/revelc/code/formatter/FormatterMojo.java
+++ src/main/java/net/revelc/code/formatter/FormatterMojo.java
@@ -139,7 +139,7 @@ public class FormatterMojo extends AbstractMojo implements ConfigurationSource {
      * 
      * @since 0.3
      */
-    @Parameter
+    @Parameter(property = "formatter.excludes")
     private String[] excludes;
 
     /**
