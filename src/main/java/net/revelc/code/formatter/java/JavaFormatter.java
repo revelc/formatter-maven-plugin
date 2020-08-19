9dd720e8ef1b1d5308b3b42299b7a40eb14076aa
Guillaume Nodet <gnodet@gmail.com>
2020-08-19 17:25:32 +0200

    Add a configuration property to allow skipping regions of the source code that match a given regular expression
    This is useful when using method chaining in DSL so that we can easily skip the portion of the java code that use the DSL.

--- src/main/java/net/revelc/code/formatter/java/JavaFormatter.java
+++ src/main/java/net/revelc/code/formatter/java/JavaFormatter.java
@@ -14,13 +14,19 @@
 package net.revelc.code.formatter.java;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.eclipse.jdt.core.ToolFactory;
 import org.eclipse.jdt.core.formatter.CodeFormatter;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.Region;
 import org.eclipse.text.edits.TextEdit;
 
 import net.revelc.code.formatter.AbstractCacheableFormatter;
@@ -30,21 +36,41 @@ import net.revelc.code.formatter.LineEnding;
 
 public class JavaFormatter extends AbstractCacheableFormatter implements Formatter {
 
+    private static final String EXCLUSION_PATTERN = "net.revelc.code.formatter.java.exclusion_pattern";
+
     private CodeFormatter formatter;
+    private Pattern exclusionPattern;
 
     @Override
     public void init(Map<String, String> options, ConfigurationSource cfg) {
         super.initCfg(cfg);
 
         this.formatter = ToolFactory.createCodeFormatter(options, ToolFactory.M_FORMAT_EXISTING);
+        String ep = options.get(EXCLUSION_PATTERN);
+        if (ep != null) {
+            exclusionPattern = Pattern.compile(ep, Pattern.MULTILINE);
+        }
     }
 
     @Override
     public String doFormat(String code, LineEnding ending) throws IOException, BadLocationException {
         TextEdit te;
         try {
-            te = this.formatter.format(CodeFormatter.K_COMPILATION_UNIT | CodeFormatter.F_INCLUDE_COMMENTS, code, 0,
-                    code.length(), 0, ending.getChars());
+            List<IRegion> regions = new ArrayList<>();
+            int start = 0;
+            if (exclusionPattern != null) {
+                Matcher matcher = exclusionPattern.matcher(code);
+                while (matcher.find()) {
+                    int s = matcher.start();
+                    int e = matcher.end();
+                    regions.add(new Region(start, s - start));
+                    start = e;
+                }
+            }
+            regions.add(new Region(start, code.length() - start));
+
+            te = this.formatter.format(CodeFormatter.K_COMPILATION_UNIT | CodeFormatter.F_INCLUDE_COMMENTS, code,
+                    regions.toArray(new IRegion[0]), 0, ending.getChars());
             if (te == null) {
                 this.log.debug(
                         "Code cannot be formatted. Possible cause is unmatched source/target/compliance version.");
