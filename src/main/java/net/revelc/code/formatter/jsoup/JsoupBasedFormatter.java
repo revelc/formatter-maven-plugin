e1512d13c2ae3e7f53a53b5e3f3f5a7c806cf11e
Jeremy Landis <jeremylandis@hotmail.com>
2022-07-24 12:10:51 -0400

    [ci] Add some clarifications to jsoup patch processing
    This is to address question on code review around line endings.  Added clarity throughout to help assist to why things exist and that normalization of line endings happens early so each call that does not do that now points back to where it was done.
    
    Effectively we take formattedCode, split it, do something with it, then recreate formattedCode so the \\r\\n only needs to occur once as its not possible afterwards.  The issue originates in jsoup and only needs the one pass.
    
    Further out builds run two times across these, if the first was to be changed, it immediately fails.  So its been vetted and checked but certainly wording was not entirely clear.

--- src/main/java/net/revelc/code/formatter/jsoup/JsoupBasedFormatter.java
+++ src/main/java/net/revelc/code/formatter/jsoup/JsoupBasedFormatter.java
@@ -69,21 +69,26 @@ public abstract class JsoupBasedFormatter extends AbstractCacheableFormatter imp
         document = Jsoup.parse(code, "", Parser.htmlParser());
         document.outputSettings(this.formatter);
 
+        // Perform Jsoup Pretty Format which does result in inconsistences handled after
         var formattedCode = document.outerHtml();
 
-        // XXX: Fixing trailing space issue caused by jsoup. We do fix this during a proper run
-        // but our tests fail to do so thus we are duplicating this until jsoup fixes bug.
+        // XXX: Trim trailing spaces inserted by jsoup. We do fix this during a full run
+        // but unit tests are direct calls thus we are duplicating this until jsoup fixes bug.
         formattedCode = REMOVE_TRAILING_PATTERN.matcher(formattedCode).replaceAll("");
 
-        // XXX: jsoup processing results in mixed line ending content and needs normalized until jsoup fixes bug.
+        // XXX: Jsoup results in mixed line ending content needing normalized until jsoup
+        // provides line ending support.  Internally Jsoup simply uses new line only and
+        // mixture comes from lines that did not require additional formatting.
         String[] lines = formattedCode.split("\\r?\\n");
         formattedCode = String.join(ending.getChars(), lines);
 
         // XXX: Fixing jsoup counter issue when more than one character indentation until jsoup fixes bug.
+        // This surfaces on some items such as <a>, <div>, <span> content with 1 extra character space.
         if (this.formatter.indentAmount() > 1) {
             int lineLength;
             int trimLineLength;
             int remainder;
+            // This is normalized on line 82/83 and simply can use our line endings
             lines = formattedCode.split(ending.getChars());
             List<String> newLines = new ArrayList<>(lines.length);
             for (String line : lines) {
@@ -101,11 +106,11 @@ public abstract class JsoupBasedFormatter extends AbstractCacheableFormatter imp
                 }
                 newLines.add(line);
             }
-            // normalizes line endings
             formattedCode = String.join(ending.getChars(), newLines);
         }
 
         // XXX: Adding new line at end of file until jsoup fixes bug.
+        // This is normalized on line 82/83 and simply can use our line endings
         lines = formattedCode.split(ending.getChars());
         if (!lines[lines.length - 1].equals(ending.getChars())) {
             formattedCode = formattedCode + ending.getChars();
